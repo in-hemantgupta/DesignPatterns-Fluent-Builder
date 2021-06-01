a27c3cb12e71e506efd8c386342dc73f643e4271
Hemant <in.hemantgupta@gmail.com>
2021-06-01 16:50:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Fluent_Builder
+{
+    class Program
+    {
+        internal class Person
+        {
+            public string Name { get; set; }
+            public string Position { get; set; }
+        }
+
+        internal class Builder : PersonJobBuilder<Builder>
+        {
+            public static Builder New() => new Builder();
+        }
+
+       internal abstract class PersonBuilder
+        {
+           internal Person person = new Person();
+
+            public override string ToString()
+            {
+                return $"Name {person.Name} , Position {person.Position}";
+            }
+
+        }
+
+        internal class PersonInfoBuilder<SELF> : PersonBuilder where SELF : PersonInfoBuilder<SELF>
+        {
+            public SELF CalledAs(string Name)
+            {
+                person.Name = Name;
+                return (SELF)this;
+            }
+        }
+
+        internal class PersonJobBuilder<SELF> : PersonInfoBuilder<SELF> where SELF : PersonJobBuilder<SELF>
+        {
+
+            public SELF WorksAs(string Position)
+            {
+                person.Position = Position;
+                return (SELF)this;
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var p = Builder.New();
+            p.CalledAs("Hemant").WorksAs("SE");
+            Console.WriteLine(p);
+            Console.Read();
+        }
+    }
+}
